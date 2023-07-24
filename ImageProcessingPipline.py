64c2c003e635910b1225198a4e7a184eb103761c
Breitkopf, Florian <Florian.Breitkopf@vector.com>
2023-07-24 19:30:10 +0200

    changed panel processing

--- ImageProcessingPipline.py
+++ ImageProcessingPipline.py
@@ -21,7 +21,7 @@ class ImageProcessingPipline:
         self.height = self.config["height"]
         self.width = self.config["width"]
         #self.color_palette = np.array([[255,255,255],[0,0,255],[0,255,0],[255,0,0],[0,0,0]]) #RGB Palette
-        self.color_palette = np.array([[255, 255, 255],[50, 53, 120],[52, 103, 78],[205, 87, 85],[236, 216, 100],[206, 120, 95],[0, 0, 0]]) #TODO ajust color palette and load it via Json
+        self.color_palette = np.array([[0, 0, 0],[255, 255, 255],[0, 255, 0],[255, 0, 0],[0, 0, 255],[0, 255, 255],[0, 128, 255]]) #TODO ajust color palette and load it via Json
         self.color_palette = self.color_palette/255
         self.path = pathlib.Path(self.storagepath)
         self.path.mkdir(parents=True, exist_ok=True)
@@ -53,17 +53,19 @@ class ImageProcessingPipline:
 
     def process_image(self,img):
         #return sucess
-
         image = self.fs_dither(img)
+        panel = self.create_array(image)
+        byteimage = self.calculate_panel_array(panel)
+        byteimage.tofile("test.csv",sep=",")
         if self.debug :
             cv2.imshow("converted Image", image)
             cv2.waitKey(0)
-
-        pass
+        return image
 
     def get_new_val(self,old_val):
         idx = np.argmin(np.sum((old_val[None,:] - self.color_palette)**2, axis=1))
-        return self.color_palette[idx]
+        return self.color_palette[idx],idx
+
 
     def fs_dither(self,img):
         """
@@ -74,13 +76,14 @@ class ImageProcessingPipline:
 
         arr = np.array(img, dtype=float) / 255
 
+
         for ir in range(self.height):
             if self.debug :
                 print("Progress"+str(100*ir/self.height)+"%")
             for ic in range(self.width):
                 # NB need to copy here for RGB arrays otherwise err will be (0,0,0)!
                 old_val = arr[ir, ic].copy()
-                new_val = self.get_new_val(old_val)
+                new_val,id = self.get_new_val(old_val)
                 arr[ir, ic] = new_val
                 err = old_val - new_val
                 # In this simple example, we will just ignore the border pixels.
@@ -97,6 +100,28 @@ class ImageProcessingPipline:
         carr = np.array(arr/np.max(arr, axis=(0,1)) * 255, dtype=np.uint8)
         return carr
 
+    def create_array(self,img):
+        """
+        Floyd-Steinberg dither the image img into a palette with nc colours per
+        channel.
+
+        """
+
+        arr = np.array(img, dtype=float) / 255
+        panel = [np.zeros((self.width,self.height)),np.zeros((self.width,self.height)),np.zeros((self.width,self.height)),np.zeros((self.width,self.height)),np.zeros((self.width,self.height)),np.zeros((self.width,self.height)),np.zeros((self.width,self.height))]
+
+
+        for ir in range(self.height):
+            if self.debug :
+                print("Progress"+str(100*ir/self.height)+"%")
+            for ic in range(self.width):
+                # NB need to copy here for RGB arrays otherwise err will be (0,0,0)!
+                old_val = arr[ir, ic].copy()
+                new_val,id = self.get_new_val(old_val)
+                panel[id][ic][ir] = 1
+                # In this simple example, we will just ignore the border pixels.
+        return panel
+
 
     def store(self,image,bin,name):
         n = pathlib.Path(name).name.strip(pathlib.Path(name).suffix)
@@ -112,8 +137,22 @@ class ImageProcessingPipline:
         bimg = self.process_image(image)
         self.store(image,bimg,img)
 
+    def calculate_panel_array(self, panel):
+        arr = np.zeros((self.height,int(self.width/2)),dtype=np.uint8)
+        for ir in range(self.height):
+            for ic in range(int(self.width/2)):
+                entry = 0
+                for i in range(len(panel)): #LSB
+                    if panel[i][ic*2][ir] > 0:
+                        entry += i
+                    if panel[i][ic*2+1][ir] > 0: #MSB
+                        print("Was geht ab")
+                        entry += (i*16) #Bitshift by 4 ;)
+                arr[ir][ic]= entry
+        print(arr)
+        return arr
 
 
 if __name__ == "__main__":
     processor = ImageProcessingPipline()
-    processor.process_and_store("test_image.jpg")
+    processor.process_and_store("test_image3.jpg")
